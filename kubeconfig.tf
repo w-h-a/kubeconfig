34a10cbd9b08ab76a5b5247af969f98bdddfe650
w-h-a <wesley.herb.anderson@gmail.com>
2024-06-02 08:35:38 -0700

    refactor: use local sensitive file

--- kubeconfig.tf
+++ kubeconfig.tf
@@ -3,9 +3,9 @@ data "digitalocean_kubernetes_cluster" "do_k8s" {
   name  = data.terraform_remote_state.k8s.outputs.cluster_name
 }
 
-resource "local_file" "do_kubeconfig" {
-  count             = var.kubernetes == "do" ? 1 : 0
-  sensitive_content = data.digitalocean_kubernetes_cluster.do_k8s[count.index].kube_config.0.raw_config
-  filename          = "${path.module}/kubeconfig"
-  file_permission   = "0600"
+resource "local_sensitive_file" "do_kubeconfig" {
+  count           = var.kubernetes == "do" ? 1 : 0
+  content         = data.digitalocean_kubernetes_cluster.do_k8s[count.index].kube_config.0.raw_config
+  filename        = "${path.module}/kubeconfig"
+  file_permission = "0600"
 }
